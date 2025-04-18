e48ea202889052bda238646adc1c52605c1fe594
Robert Kramer <rwkramer@ysu.edu>
2025-04-18 13:27:27 -0400

    added project 4 starter project

---
+++ Projects/4/cmake-build-debug/CMakeFiles/Solution.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bob/School/Courses/3700.course/Projects/BSTDictionary/Solution/main.cpp" "CMakeFiles/Solution.dir/main.cpp.o" "gcc" "CMakeFiles/Solution.dir/main.cpp.o.d"
+  "/home/bob/School/Courses/3700.course/Projects/BSTDictionary/Solution/sampler.cpp" "CMakeFiles/Solution.dir/sampler.cpp.o" "gcc" "CMakeFiles/Solution.dir/sampler.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
